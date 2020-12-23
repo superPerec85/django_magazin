bdf26c91d12915a6494a10183abc93dbf7efa660
Rinat <m3.rinat@gmail.com>
2020-12-23 19:13:08 +0300

    lesson4

--- geekshop/authapp/urls.py
+++ geekshop/authapp/urls.py
@@ -5,6 +5,8 @@ import authapp.views as authapp
 app_name = 'authapp'
 
 urlpatterns = [
+    path('register/', authapp.register, name='register'),
     path('login/', authapp.login, name='login'),
+    path('edit/', authapp.edit, name='edit'),
     path('logout/', authapp.logout, name='logout'),
 ]
